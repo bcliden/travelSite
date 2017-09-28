df14b25c642c4af0f28e125d70e858bade4b3eac
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-09-28 16:47:23 -0400

    es6 intro files cleaned up. Clean slate in app/assets/scripts.

--- app/assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-var Person = require("./modules/Person"),
-   $ = require("jquery");
-
-var john = new Person("John Doe", "Yellow");
-var jane = new Person("Jane Smith", "Grench");
-
-john.greet();
-jane.greet();
--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(name, color) {
-   this.name = name;
-   this.favColor = color;
-   this.greet = function(){
-      console.log("Oh hello! My name is " + this.name + " and " + this.favColor + " is my favorite color.");
-   }
-}
-
-module.exports = Person;
